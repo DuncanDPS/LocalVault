cfdf4551b48296a92d4d8b4fb97ca447b6068c1f
Duncan <duncandmp12@gmail.com>
2026-01-29 19:09:23 -0600

    Refactor ICategoriaService to define interface for category logic and add method documentation

--- NubeCasera/Servicios/ICategoriaService.cs
+++ NubeCasera/Servicios/ICategoriaService.cs
@@ -1,8 +1,21 @@
 using System;
+using NubeCasera.Dtos;
 
 namespace NubeCasera.Servicios;
 
-public class ICategoriaService
+/// <summary>
+/// Interfaz que define los metodos de la logica para las carpetas logicas
+/// </summary>
+public interface ICategoriaService
 {
-    
+    /// <summary>
+    /// Crea una categoria nueva
+    /// </summary>
+    /// <param name="categoriaNueva">Obj categoria nueva</param>
+    /// <returns></returns>
+    // TODO: IMPLEMENTAR ESTE METODO
+    public Task<CategoriaDTO> CrearCategoriaAsync(CategoriaDTO_Add categoriaNueva);
+
+
+
 }
