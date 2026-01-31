e7cfef76ed91f62330fc98dff35470fd12f730db
Duncan <duncandmp12@gmail.com>
2026-01-31 15:34:30 -0600

    Implement ICategoriaService in CategoriaService and update dependency injection in Program.cs

--- NubeCasera/Servicios/CategoriaService.cs
+++ NubeCasera/Servicios/CategoriaService.cs
@@ -1,8 +1,28 @@
 using System;
-
+using NubeCasera.Dtos;
+using NubeCasera.Datos;
 namespace NubeCasera.Servicios;
 
-public class CategoriaService
+public class CategoriaService : ICategoriaService
 {
 
+    private readonly AppDBContext _appDbContext;
+
+    public CategoriaService(AppDBContext appDBContext)
+    {
+        _appDbContext = appDBContext;
+    }
+
+    public Task<CategoriaDTO> CrearCategoriaAsync(CategoriaDTO_Add categoriaNueva)
+    {
+        // valida que la categoria no este vacia
+        if(categoriaNueva == null)
+        {
+            throw new ArgumentNullException(nameof(categoriaNueva));
+        }
+
+
+
+
+    }
 }
